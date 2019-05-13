1b08fff8ccca2f5633063abf748b690c20652468
drewatienza <atienza.drew@gmail.com>
2019-05-13 11:29:26 -0700

    inquiry check and send email

--- realestate/settings.py
+++ realestate/settings.py
@@ -141,3 +141,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'primo6150@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL__USE_TLS=True
